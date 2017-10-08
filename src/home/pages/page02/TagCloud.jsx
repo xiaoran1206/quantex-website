ddf5e391119fd7137067ffc7efa88fa148e69071
sharon <sharon.wang@iquantex.com>
2017-10-08 12:41:29 +0800

    添加云标签文本

--- src/home/pages/page02/TagCloud.jsx
+++ src/home/pages/page02/TagCloud.jsx
@@ -11,19 +11,81 @@ class TagCloudComponent extends Component {
 
   render() {
     return (
-        <div id="div1">
-          <a href="http://www.jq22.com">智能化</a>
-          <a href="http://www.jq22.com/" className="red">智能化</a>
-          <a href="http://www.jq22.com">智能投顾</a>
-          <a href="http://www.jq22.com">智能投顾</a>
-          <a href="http://www.jq22.com" className="blue">场景化</a>
-          <a href="http://www.jq22.com">智能投顾</a>
-          <a href="http://www.jq22.com" className="red">智能投顾</a>
-          <a href="http://www.jq22.com/" className="yellow">知识图谱</a>
-          <a href="http://www.jq22.com/">风险预警</a>
-          <a href="http://www.jq22.com/" className="red">个性化定制</a>
-          <a href="http://www.jq22.com/">个性化定制</a>
-        </div>
+      <div id="div1">
+        <a className="red">智能化</a>
+        <a>集约管理</a>
+        <a>操作便捷</a>
+        <a>数据处理</a>
+        <a>动态调整</a>
+        <a>实时跟踪</a>
+        <a>快速识别</a>
+        <a>自动交易</a>
+        <a>在线分析</a>
+        <a>高效统计</a>
+        <a>兼容拓展</a>
+        <a>可视化展示</a>
+        <a>可视化展示</a>
+        <a>业务集成</a>
+        <a>平台灵活</a>
+        <a>安全稳定</a>
+        <a>PB数据</a>
+        <a>结构化处理</a>
+        <a>清洗挖掘</a>
+        <a>分析优化</a>
+        <a>动态识别</a>
+        <a>相关因子</a>
+        <a className="blue">个性化</a>
+        <a>智能投顾</a>
+        <a>标准服务</a>
+        <a>信息同步</a>
+        <a>行为分析</a>
+        <a>知识图谱</a>
+        <a>风险预警</a>
+        <a>本地化部署</a>
+        <a>私有化操作</a>
+        <a>灵活配置</a>
+        <a>多元协同</a>
+        <a>千人千面</a>
+        <a>用户画像</a>
+        <a>精准营销</a>
+        <a>多账户管理</a>
+        <a>数据应用</a>
+        <a>全量数据</a>
+        <a>量化策略</a>
+        <a>需求驱动</a>
+        <a>聚类有序</a>
+        <a>因子特征</a>
+        <a>策略有效</a>
+        <a>择时调整</a>
+        <a>舆情监控</a>
+        <a>客观决策</a>
+        <a>个性化定制</a>
+        <a className="yellow">场景化</a>
+        <a>高端咨询</a>
+        <a>垂直聚焦</a>
+        <a>场景连接</a>
+        <a>精简整合</a>
+        <a>流程疏通</a>
+        <a>业务协同</a>
+        <a>专业能力</a>
+        <a>行业认知</a>
+        <a>精准诊断</a>
+        <a>实操经验</a>
+        <a>沟通便捷</a>
+        <a>管家服务</a>
+        <a>行业口碑</a>
+        <a>品牌信誉</a>
+        <a>按需治理</a>
+        <a>人才储备</a>
+        <a>注重创新</a>
+        <a>供需驱动</a>
+        <a>合规监管</a>
+        <a>聚焦业务</a>
+        <a>成本低廉</a>
+        <a>规模效应</a>
+        <a>普惠服务</a>
+        <a>提升效率</a>
+      </div>
     )
   }
 }
