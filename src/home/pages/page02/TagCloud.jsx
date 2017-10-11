0e5f4f149643b723ff830418aa09928696b813cb
sharon <sharon.wang@iquantex.com>
2017-10-11 23:28:41 +0800

    添加颜色

--- src/home/pages/page02/TagCloud.jsx
+++ src/home/pages/page02/TagCloud.jsx
@@ -12,7 +12,7 @@ class TagCloudComponent extends Component {
   render() {
     return (
       <div id="tag-cloud">
-        <a className="dark-blue" style={{ fontSize: '20px'}}>智能化</a>
+        <a className="c-39a2d9" style={{ fontSize: '36px'}}>智能化</a>
         <a className="dark-blue">集约管理</a>
         <a className="dark-green">操作便捷</a>
         <a className="green">数据处理</a>
@@ -34,53 +34,53 @@ class TagCloudComponent extends Component {
         <a className="light-blue">分析优化</a>
         <a className="light-green">动态识别</a>
         <a className="light-green">相关因子</a>
-        <a className="blue" style={{ fontSize: '20px'}}>个性化</a>
+        <a className="c-76bcfa" style={{ fontSize: '36px'}}>个性化</a>
         <a className="light-green">智能投顾</a>
         <a className="dark-green">标准服务</a>
         <a className="light-green">信息同步</a>
         <a className="green">行为分析</a>
         <a className="light-green">知识图谱</a>
         <a className="light-blue">风险预警</a>
-        <a>本地化部署</a>
+        <a className="c-a1ceeb">本地化部署</a>
         <a className="green">私有化操作</a>
         <a className="dark-blue">灵活配置</a>
         <a className="light-green">多元协同</a>
         <a className="blue">千人千面</a>
-        <a>用户画像</a>
+        <a className="c-56b1e6">用户画像</a>
         <a className="light-green">精准营销</a>
         <a className="green">多账户管理</a>
         <a className="dark-blue">数据应用</a>
-        <a>全量数据</a>
+        <a className="c-dbe0f3">全量数据</a>
         <a className="dark-green">量化策略</a>
         <a className="light-green">需求驱动</a>
         <a className="light-blue">聚类有序</a>
         <a className="dark-green">因子特征</a>
         <a className="green">策略有效</a>
-        <a className="red">择时调整</a>
+        <a className="light-green">择时调整</a>
         <a className="dark-blue">舆情监控</a>
         <a className="dark-green">客观决策</a>
-        <a>个性化定制</a>
-        <a className="yellow" style={{ fontSize: '20px'}}>场景化</a>
-        <a>高端咨询</a>
+        <a className="c-6db7e4">个性化定制</a>
+        <a className="c-88d8e3" style={{ fontSize: '36px'}}>场景化</a>
+        <a className="purple">高端咨询</a>
         <a className="light-blue">垂直聚焦</a>
-        <a>场景连接</a>
+        <a className="c-6bb8e6">场景连接</a>
         <a className="light-green">精简整合</a>
-        <a>流程疏通</a>
+        <a className="purple">流程疏通</a>
         <a className="blue">业务协同</a>
-        <a>专业能力</a>
+        <a className="c-a1d0ec">专业能力</a>
         <a className="dark-green">行业认知</a>
-        <a>精准诊断</a>
+        <a className="c-6bb8e6">精准诊断</a>
         <a className="light-blue">实操经验</a>
         <a className="green">沟通便捷</a>
-        <a>管家服务</a>
+        <a className="c-56b1e6">管家服务</a>
         <a className="dark-blue">行业口碑</a>
         <a className="dark-green">品牌信誉</a>
         <a className="green">按需治理</a>
         <a className="blue">人才储备</a>
-        <a>注重创新</a>
+        <a className="c-6bb8e6">注重创新</a>
         <a className="light-blue">供需驱动</a>
         <a className="dark-green">合规监管</a>
-        <a>聚焦业务</a>
+        <a className="dark-green">聚焦业务</a>
         <a className="dark-blue">成本低廉</a>
         <a className="blue">规模效应</a>
         <a className="light-green">普惠服务</a>
